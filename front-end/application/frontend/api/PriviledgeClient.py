1e3b516c304b2c6bee8a0eec7a2ad3c1f7a95d66
Fashaan <fashan@datafields.net>
2022-07-24 23:17:59 +0200

    fashan-dynamic nav bar

--- front-end/application/frontend/api/PriviledgeClient.py
+++ front-end/application/frontend/api/PriviledgeClient.py
@@ -2,8 +2,14 @@ import requests
 
 class PrivilegeClient:
     @staticmethod
-    def group_sections():
-        r = requests.get('http://127.0.0.1:5002/api/get-section-postion')
+    def group_sections(user_id):
+        r = requests.get(f'http://127.0.0.1:5002/api/get-section-postion/{user_id}')
         sections = r.json()
         return sections
+
+    @staticmethod
+    def get_sub_sections(user_id, section_id):
+        r = requests.get(f'http://127.0.0.1:5002/api/get-subsection/{user_id}/{section_id}')
+        sub_sections = r.json()
+        return sub_sections
 
