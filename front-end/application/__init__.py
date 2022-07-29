7f04afe9d6c2d0a626926c55ed7015c11f73ebff
Fashaan <fashan@datafields.net>
2022-07-29 20:49:52 +0200

    school_service file contains sqls queries of the application. (Database)

--- front-end/application/__init__.py
+++ front-end/application/__init__.py
@@ -11,6 +11,7 @@ UPLOAD_FOLDER = 'application/static/images'
 def create_app():
     app = Flask(__name__, static_folder='static')
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    # app.register_error_handler(404, page_not_found)
 
     environment_configuration = os.environ['CONFIGURATION_SETUP']
     app.config.from_object(environment_configuration)
